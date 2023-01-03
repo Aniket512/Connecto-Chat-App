41ee8aa6cd3c76662e2a7091f8b83b37d8d05133
Aniket Mishra <aniketmishra2709@gmail.com>
2023-01-03 13:01:39 +0530

    build added

---
+++ client/build/precache-manifest.9d9db84ee7f87774cdbcd4b6b7cd6169.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1f95a1384bd785fede9",
+    "url": "/static/css/main.ca3520e7.chunk.css"
+  },
+  {
+    "revision": "a1f95a1384bd785fede9",
+    "url": "/static/js/main.899a716d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "458faf8e1d1324862f41",
+    "url": "/static/css/2.d9be7c61.chunk.css"
+  },
+  {
+    "revision": "458faf8e1d1324862f41",
+    "url": "/static/js/2.c15ef317.chunk.js"
+  },
+  {
+    "revision": "8de4c62a383babfce8fd7f2b3ce3c87e",
+    "url": "/static/media/logo.8de4c62a.png"
+  },
+  {
+    "revision": "e1a4f2728c50cdb05990b98d7c527179",
+    "url": "/static/media/loader.e1a4f272.gif"
+  },
+  {
+    "revision": "3b7bae9770f0c452cf9eb4c8a19ef2f3",
+    "url": "/static/media/robot.3b7bae97.gif"
+  },
+  {
+    "revision": "07f247703adb71b193882d9dad29f0e1",
+    "url": "/index.html"
+  }
+];
